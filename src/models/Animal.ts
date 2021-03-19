173fb0781ae93213c99fd0920faacda3c6285040
Sub-Xaero <Sub-Xaero@users.noreply.github.com>
2021-03-19 21:07:34 +0000

    Give all Animals IDs

--- src/models/Animal.ts
+++ src/models/Animal.ts
@@ -1,10 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+
 // Abstract Base Class
 // This model and all sub-classes use polymorphism, to easily share common properties between the sub-classes of animal and provide a common interface for the rendering logic.
 export abstract class Animal {
 
+  id: string;
   health: number = 100;
   dead: boolean = false;
   injured: boolean = false;
+
+  constructor() {
+    this.id = uuidv4();
+  }
 
   abstract get image(): any;
 
